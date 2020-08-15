0199e18c23d4e227bcdf82e8b5bc26e08e16ad86
Oscar Davila <oscar7822002@gmail.com>
2020-08-15 08:41:43 -0500

    Agregando icono a la app

--- CompudavSystem/Properties/AssemblyInfo.cs
+++ CompudavSystem/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("CompudavSystem")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Aplicacion de escritorio legacy para facturación.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CompudavSystem")]
